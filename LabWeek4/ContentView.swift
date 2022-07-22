51dff01b616e29126cb5b37940e03b3fa0b89540
tnguy753 <nguyenthao989@gmail.com>
2022-07-22 13:10:28 +0700

    Initial Commit

---
+++ LabWeek4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LabWeek4
+//
+//  Created by Nguyễn Thảo on 22/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LabWeek4/LabWeek4App.swift
@@ -0,0 +1,17 @@
+//
+//  LabWeek4App.swift
+//  LabWeek4
+//
+//  Created by Nguyễn Thảo on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LabWeek4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
